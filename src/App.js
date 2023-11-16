42d964c3861a83f9289068fb7602e0d3066ddb9a
GacheruIan <gacheruian99@gmail.com>
2023-11-17 00:17:11 +0300

    saving work here lol

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Contact from './Contact'
+
+const App = () => {
+  return (
+    <div>
+      {/* Other content */}
+      <Contact />
+      {/* More content */}
+    </div>
+  );
+};
+
+export default App
